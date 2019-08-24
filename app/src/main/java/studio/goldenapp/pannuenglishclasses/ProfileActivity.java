a8fbc688ff38c33c080a3107c0502d4f85cf2c96
HW-Faction <hankwilliamson49@gmail.com>
2019-08-24 15:59:29 +0530

    v2.1.0

--- app/src/main/java/studio/goldenapp/pannuenglishclasses/ProfileActivity.java
+++ app/src/main/java/studio/goldenapp/pannuenglishclasses/ProfileActivity.java
@@ -1,55 +1,148 @@
 package studio.goldenapp.pannuenglishclasses;
 
+import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ProfileActivity extends AppCompatActivity {
 
-    FirebaseAuth mAuth;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseFirestore firebaseFirestore;
     private Button continueRegister ;
-    private EditText name;
+    private EditText fullname;
     private EditText age;
     private EditText email;
     private EditText place;
     private EditText gender;
-
-
+    private String user_id;
+    private boolean isChanged = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(1);
-        if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.LOLLIPOP){
-            getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
             getWindow().setStatusBarColor(Color.TRANSPARENT);
         }
         setContentView(R.layout.activity_profile);
 
-        mAuth = FirebaseAuth.getInstance();
         continueRegister = findViewById(R.id.continueRegister);
-        name = findViewById(R.id.fullname);
+        fullname = findViewById(R.id.fullname);
         age = findViewById(R.id.age);
         email = findViewById(R.id.email);
         gender = findViewById(R.id.gender);
         place = findViewById(R.id.birthpalce);
-        name = findViewById(R.id.fullname);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        user_id = firebaseAuth.getCurrentUser().getUid();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+
+
+        firebaseFirestore.collection("Users").document(user_id).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+
+                if (task.isSuccessful()) {
+
+                    if (task.getResult().exists()) {
+
+                        String name = task.getResult().getString("name");
+                        String ageOFName = task.getResult().getString("age");
+                        String mail = task.getResult().getString("mail");
+                        String address = task.getResult().getString("address");
+                        String genderOfName = task.getResult().getString("gender");
+
+                        fullname.setText(name);
+                        age.setText(ageOFName);
+                        email.setText(mail);
+                        place.setText(address);
+                        gender.setText(genderOfName);
+
+                    }
+
+                } else {
+
+                    String error = task.getException().getMessage();
+                    Toast.makeText(ProfileActivity.this, "(FIRESTORE Retrieve Error) : " + error, Toast.LENGTH_LONG).show();
+
+                }
+
+            }
+        });
 
 
         continueRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                final String user_name = fullname.getText().toString();
+                final String user_address = place.getText().toString();
+                final String user_email = email.getText().toString();
+                final String user_age = age.getText().toString();
+                final String user_gender = gender.getText().toString();
 
+                if (!TextUtils.isEmpty(user_name) && !TextUtils.isEmpty(user_gender) && (!TextUtils.isEmpty(user_address)) && (!TextUtils.isEmpty(user_age))) {
+
+
+                    storeFirestore(user_name, user_email, user_address, user_age, user_gender);
+
+                }
             }
         });
 
     }
+
+        private void storeFirestore(String user_name, String user_gender, String user_age, String user_address, String user_email) {
+
+            Map<String, String> userMap = new HashMap<>();
+            userMap.put("name", user_name);
+            userMap.put("gender", user_gender);
+            userMap.put("address", user_address);
+            userMap.put("age", user_age);
+            userMap.put("mail", user_email);
+
+            firebaseFirestore.collection("Users").document(user_id).set(userMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+
+                    if(task.isSuccessful()){
+
+                        Toast.makeText(ProfileActivity.this, "The user Settings are updated.", Toast.LENGTH_LONG).show();
+                        Intent mainIntent = new Intent(ProfileActivity.this, MainActivity.class);
+                        startActivity(mainIntent);
+                        finish();
+
+                    } else {
+
+                        String error = task.getException().getMessage();
+                        Toast.makeText(ProfileActivity.this, "(FIRESTORE Error) : " + error, Toast.LENGTH_LONG).show();
+
+                    }
+
+                }
+            });
+
+
+        }
+
 }
