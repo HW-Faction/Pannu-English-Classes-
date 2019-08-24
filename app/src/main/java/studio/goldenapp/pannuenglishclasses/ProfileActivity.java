0d5fe3e79cbc6e6bff383a53a1d098251aaa70a6
HW-Faction <hankwilliamson49@gmail.com>
2019-08-24 14:12:50 +0530

    v2.0.3

--- app/src/main/java/studio/goldenapp/pannuenglishclasses/ProfileActivity.java
+++ app/src/main/java/studio/goldenapp/pannuenglishclasses/ProfileActivity.java
@@ -1,16 +1,11 @@
 package studio.goldenapp.pannuenglishclasses;
 
-
-import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.Spinner;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -20,6 +15,8 @@ public class ProfileActivity extends AppCompatActivity {
 
     FirebaseAuth mAuth;
     private Button continueRegister ;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,11 +31,6 @@ public class ProfileActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         continueRegister = findViewById(R.id.continueRegister);
 
-        Spinner spinner = findViewById(R.id.planets_spinner);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.planets_array, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
 
         continueRegister.setOnClickListener(new View.OnClickListener() {
             @Override
