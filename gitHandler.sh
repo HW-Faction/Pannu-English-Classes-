76f58a0526120b264181bf9ddd3052ae140cbbd6
HW-Faction <hankwilliamson49@gmail.com>
2019-08-24 12:34:54 +0530

    v1.2.1

--- gitHandler.sh
+++ gitHandler.sh
@@ -2,22 +2,22 @@ echo starting git daemon.......
 git add --all
 git commit -m "v1.2.1" .
 git push origin master
-sleep 10000
+sleep 200
 echo 2nd 
 git add --all
 git commit -m "v1.2.2" .
 git push origin master
-sleep 10000
+sleep 200
 echo 3rd
 git add --all
 git commit -m "v1.2.3" .
 git push origin master
-sleep 10000
+sleep 200
 echo 4th
 git add --all
 git commit -m "v1.2.4" .
 git push origin master
-sleep 10000
+sleep 200
 echo 5th
 git add -all
 git commit -m "v1.2.5" .
